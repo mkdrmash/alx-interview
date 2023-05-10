7eb2f090aaf132d5893a5317d1316d311060e403
Mohammed Kedir <mkdrmash@gmail.com>
2023-05-10 15:23:42 +0300

    0x03-log_parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,54 +1,41 @@
 #!/usr/bin/python3
-
+"""Log Parsing
+Write a script that reads stdin line by line and computes metrics:
+"""
 import sys
 
 
-def print_msg(dict_sc, total_file_size):
-    """
-    Method to print
-    Args:
-        dict_sc: dict of status codes
-        total_file_size: total of the file
-    Returns:
-        Nothing
-    """
-
-    print("File size: {}".format(total_file_size))
-    for key, val in sorted(dict_sc.items()):
-        if val != 0:
-            print("{}: {}".format(key, val))
-
-
 total_file_size = 0
-code = 0
-counter = 0
-dict_sc = {"200": 0,
-           "301": 0,
-           "400": 0,
-           "401": 0,
-           "403": 0,
-           "404": 0,
-           "405": 0,
-           "500": 0}
+status = ['200', '301', '400', '401', '403', '404', '405', '500']
+obj = dict.fromkeys(status, 0)
 
-try:
-    for line in sys.stdin:
-        parsed_line = line.split()  # ✄ trimming
-        parsed_line = parsed_line[::-1]  # inverting
 
-        if len(parsed_line) > 2:
-            counter += 1
+def printLogStat():
+    """Print log statistics"""
+    print("File size: {}".format(total_file_size))
+    for key, value in sorted(obj.items()):
+        if value > 0:
+            print("{}: {}".format(key, value))
 
-            if counter <= 10:
-                total_file_size += int(parsed_line[0])  # file size
-                code = parsed_line[1]  # status code
 
-                if (code in dict_sc.keys()):
-                    dict_sc[code] += 1
+if __name__ == "__main__":
+    count = 0
+    try:
+        for line in sys.stdin:
+            line = line.split()
+            count += 1
+            try:
+                total_file_size += int(line[-1])
 
-            if (counter == 10):
-                print_msg(dict_sc, total_file_size)
-                counter = 0
+                if line[-2] in status:
+                    obj[line[-2]] += 1
 
-finally:
-    print_msg(dict_sc, total_file_size)
+            except (IndexError, ValueError):
+                pass
+
+            if count % 10 == 0:
+                printLogStat()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        printLogStat()
